a615aca0e8d887798f7015a034f4358d7c5a1e4f
heikeee <kouma2500@icloud.com>
2023-11-07 17:58:25 +0900

    入力画面を変更した

--- app/my-app-ts/src/App.tsx
+++ app/my-app-ts/src/App.tsx
@@ -4,25 +4,29 @@ import './App.css';
 import LoginForm from './Loginform';
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <LoginForm />
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+
+                <div className="login-form-container">
+                    <LoginForm />
+                </div>
+
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
